7683c4c1195c6f41514ceba9ee98926165407af3
Nikolay Chapkin <nic-ch-work@yandex.ru>
2019-04-06 11:44:54 +0300

    Общие интерфейсы и начало движка

---
+++ DoDoHackatonEngine/Common/IMovingAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿namespace Common
+{
+    public enum Direction
+    {
+        NorthWest = 1,
+        West = 2,
+        SouthWest = 3,
+        SouthEast = 4,
+        East = 5,
+        NorthEast = 6
+    }
+
+    public enum HexType
+    {
+        Unknown = 0,
+        Empty = 1,
+        Rock = 2,
+        DangerousArea = 3,
+        Pit = 4
+    }
+
+    interface IMovingAlgorithm
+    {
+        void AddHexes(HexType[] hexes);
+
+        (Direction, int) WhereToGo(Direction currentDirection, int currentVelocity);
+    }
+}
---
+++ DoDoHackatonEngine/DoDoHackatonEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoDoHackatonEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
